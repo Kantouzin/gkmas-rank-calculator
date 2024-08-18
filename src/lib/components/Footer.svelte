7ed8786e539e7520673e5b2303e2da623db09026
Kantouzin <kantouzin0113@gmail.com>
2024-08-18 13:10:58 +0900

    Add i18n

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,13 @@
+<script>
+  import { _ } from 'svelte-i18n';
+</script>
+
 <footer class="footer bg-secondary mt-auto">
   <div class="container text-center text-light">
     <p class="m-2">
-      <i class="bi bi-c-circle"></i><span class="ms-1">Kantouzin</span>
+      <i class="bi bi-c-circle"></i><span class="ms-1">
+        {$_('footer.author')}
+      </span>
     </p>
   </div>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,11 @@
+<script>
+  import { _ } from 'svelte-i18n';
+</script>
+
 <nav class="navbar navbar-dark bg-dark">
   <div class="container flex-nowrap">
-    <h1 class="navbar-brand fs-5 text-light">学マス評価計算機</h1>
+    <h1 class="navbar-brand fs-5 text-light">
+      {$_('header.title')}
+    </h1>
   </div>
 </nav>
