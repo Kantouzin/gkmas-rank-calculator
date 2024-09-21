d6952cfc52281cc17fe34fa577f085952c803353
Kantouzin <kantouzin0113@gmail.com>
2024-09-21 21:03:04 +0900

    Fix a calculation error

--- src/lib/modules/calculator.js
+++ src/lib/modules/calculator.js
@@ -1,32 +1,33 @@
 import ranks from '../../json/ranks.json';
+import Big from 'big.js';
 
 const calculateTargetScore = (targetRankValue = 0, voValue = 0, daValue = 0, viValue = 0) => {
-  const statusValue = Math.floor(
-    (Math.min(voValue + 30, 1800) +
-      Math.min(daValue + 30, 1800) +
-      Math.min(viValue + 30, 1800)) *
-    2.3,
-  );
-  const targetValue = targetRankValue - statusValue - 1700;
+  const statusValue = new Big(0)
+    .plus(Math.min(voValue + 30, 1800))
+    .plus(Math.min(daValue + 30, 1800))
+    .plus(Math.min(viValue + 30, 1800))
+    .times('2.3')
+    .round(0, Big.roundDown);
 
-  let targetScore = 0;
-  if (targetValue <= 1500) {
-    targetScore = targetValue / 0.3;
-  } else if (targetValue <= 2250) {
-    targetScore = 5000 + (targetValue - 1500) / 0.15;
-  } else if (targetValue <= 3050) {
-    targetScore = 10000 + (targetValue - 2250) / 0.08;
-  } else if (targetValue <= 3450) {
-    targetScore = 20000 + (targetValue - 3050) / 0.04;
-  } else if (targetValue <= 3650) {
-    targetScore = 30000 + (targetValue - 3450) / 0.02;
+  const targetValue = new Big(targetRankValue).minus(statusValue).minus(1700);
+
+  let targetScore = new Big(0);
+  if (targetValue.lte(1500)) {
+    targetScore = targetValue.div('0.3');
+  } else if (targetValue.lte(2250)) {
+    targetScore = targetValue.minus(1500).div('0.15').plus(5000);
+  } else if (targetValue.lte(3050)) {
+    targetScore = targetValue.minus(2250).div('0.08').plus(10000);
+  } else if (targetValue.lte(3450)) {
+    targetScore = targetValue.minus(3050).div('0.04').plus(20000);
+  } else if (targetValue.lte(3650)) {
+    targetScore = targetValue.minus(3450).div('0.02').plus(30000);
   } else {
-    targetScore = 40000 + (targetValue - 3650) / 0.01;
+    targetScore = targetValue.minus(3650).div('0.01').plus(40000);
   }
+  targetScore = targetScore.round(0, Big.roundDown);
 
-  targetScore = Math.floor(targetScore);
-
-  return targetScore > 0 ? targetScore : 0;
+  return targetScore.gt(0) ? targetScore.toNumber() : 0;
 };
 
 const calculateRankValue = (score, voValue = 0, daValue = 0, viValue = 0) => {
@@ -34,30 +35,30 @@ const calculateRankValue = (score, voValue = 0, daValue = 0, viValue = 0) => {
     return;
   }
 
-  let scoreRankValue = 0;
+  let scoreRankValue = new Big(0);
   if (score <= 5000) {
-    scoreRankValue = score * 0.3;
+    scoreRankValue = scoreRankValue.plus(score).times('0.3');
   } else if (score <= 10000) {
-    scoreRankValue = score * 0.15 + 750;
+    scoreRankValue = scoreRankValue.plus(score).times('0.15').plus(1500);
   } else if (score <= 20000) {
-    scoreRankValue = (score - 10000) * 0.08 + 2250;
+    scoreRankValue = scoreRankValue.plus(score).minus(10000).times('0.08').plus(2250);
   } else if (score <= 30000) {
-    scoreRankValue = (score - 20000) * 0.04 + 3050;
+    scoreRankValue = scoreRankValue.plus(score).minus(20000).times('0.04').plus(3050);
   } else if (score <= 40000) {
-    scoreRankValue = (score - 30000) * 0.02 + 3450;
+    scoreRankValue = scoreRankValue.plus(score).minus(30000).times('0.02').plus(3450);
   } else {
-    scoreRankValue = (score - 40000) * 0.01 + 3650;
+    scoreRankValue = scoreRankValue.plus(score).minus(40000).times('0.01').plus(3650);
   }
-  scoreRankValue = Math.round(scoreRankValue);
+  scoreRankValue = scoreRankValue.round(0, Big.roundDown);
 
-  const statusRankValue = Math.floor(
-    (Math.min(voValue + 30, 1800) +
-      Math.min(daValue + 30, 1800) +
-      Math.min(viValue + 30, 1800)) *
-    2.3,
-  );
+  const statusRankValue = new Big(0)
+    .plus(Math.min(voValue + 30, 1800))
+    .plus(Math.min(daValue + 30, 1800))
+    .plus(Math.min(viValue + 30, 1800))
+    .times('2.3')
+    .round(0, Big.roundDown);
 
-  return scoreRankValue + statusRankValue + 1700;
+  return scoreRankValue.plus(statusRankValue).plus(1700).toNumber();
 };
 
 const getRank = (rankValue) => {
